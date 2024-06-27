816cfd880567a409a8695e2fdd1b94631a99484b
Vlad <vladyslav.linnik@lnu.edu.ua>
2024-06-28 00:25:38 +0300

    Initial commit

---
+++ TechShop/Initializer.cs
@@ -0,0 +1,26 @@
+﻿namespace TechShop
+{
+    public static class Initializer
+    {
+        public static async Task InitializeRepositories(this IServiceCollection services)
+        {
+
+
+            await Task.CompletedTask;
+        }
+
+        public static async Task InitializeServices(this IServiceCollection services)
+        {
+
+
+            await Task.CompletedTask;
+        }
+
+        public static async Task InitializeSwagger(this IServiceCollection services)
+        {
+
+
+            await Task.CompletedTask;
+        }
+    }
+}
---
+++ TechShop/Program.cs
@@ -0,0 +1,32 @@
+using TechShop;
+
+var builder = WebApplication.CreateBuilder(args);
+
+await builder.Services.InitializeRepositories();
+await builder.Services.InitializeServices();
+await builder.Services.InitializeSwagger();
+
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+await app.RunAsync();
