52c4e6346cd75002778d545c3fcacab767eff928
Vlad <vladyslav.linnik@lnu.edu.ua>
2024-07-08 00:19:00 +0300

    merge fix

---
+++ TechShop.Domain/DTOs/AdminDto/ResponseAdminDto.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+using TechShop.Domain.DTOs.ProductDtos.ProductCaregoryDto;
+using TechShop.Domain.DTOs.ProductDtos.ProductDto;
+using TechShop.Domain.DTOs.UserDtos.UserDto;
+
+namespace TechShop.Domain.DTOs.AdminDto
+{
+    public class ResponseAdminDto
+    {
+        public IEnumerable<ApplicationUserDto> Users { get; set; } = [];
+        public IEnumerable<IdentityRole> Roles { get; set; } = [];
+        public IEnumerable<ResponseProductDto> Products { get; set; } = [];
+        public IEnumerable<ResponseProductCaregoryDto> Categories { get; set; } = [];
+    }
+}
---
+++ TechShop/Controllers/AdminController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using TechShop.Application.Services.AdminService;
+
+namespace TechShopWeb.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    [TypeFilter(typeof(MvcControllerExceptionFilter))]
+    public class AdminController(IAdminService adminService) : Controller
+    {
+        [HttpGet]
+        public IActionResult AdminPanel()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult GetUsersAdminPanel()
+        {
+            var response = adminService.GetAdminPanel().Users;
+            return PartialView("~/Views/Admin/_UsersPartial.cshtml", response);
+        }
+
+        [HttpGet]
+        public IActionResult GetProductsAdminPanel()
+        {
+            var response = adminService.GetAdminPanel().Products;
+            return PartialView("~/Views/Admin/_ProductsPartial.cshtml", response);
+        }
+
+        [HttpGet]
+        public IActionResult GetCategoriesAdminPanel()
+        {
+            var response = adminService.GetAdminPanel().Categories;
+            return PartialView("~/Views/Admin/_CategoriesPartial.cshtml", response);
+        }
+    }
+}
